746eb0eeee116458b51d3bd7339bc8bb6704eb02
Clem Fern <clemfern456@gmail.com>
2024-12-19 23:43:10 +0100

    fix onSignBreak NullPointerException on "InventoryHolder.getInventory()" because "holder" is null

--- src/main/java/org/gestern/gringotts/AccountChest.java
+++ src/main/java/org/gestern/gringotts/AccountChest.java
@@ -97,15 +97,18 @@ public class AccountChest {
 
     public boolean matchesLocation(Location loc) {
         if (this.containerLocations.isEmpty()) {
+            containerLocations.add(sign.getLocation());
+
             InventoryHolder holder = chest();
-
-            if (holder.getInventory() instanceof DoubleChestInventory) {
-                DoubleChestInventory doubleChest = (DoubleChestInventory) holder.getInventory();
-
-                containerLocations.add(doubleChest.getLeftSide().getLocation());
-                containerLocations.add(doubleChest.getRightSide().getLocation());
-            } else {
-                containerLocations.add(holder.getInventory().getLocation());
+            if (holder != null) {
+                if (holder.getInventory() instanceof DoubleChestInventory) {
+                    DoubleChestInventory doubleChest = (DoubleChestInventory) holder.getInventory();
+    
+                    containerLocations.add(doubleChest.getLeftSide().getLocation());
+                    containerLocations.add(doubleChest.getRightSide().getLocation());
+                } else {
+                    containerLocations.add(holder.getInventory().getLocation());
+                }
             }
         }
         loc = loc.toBlockLocation();
